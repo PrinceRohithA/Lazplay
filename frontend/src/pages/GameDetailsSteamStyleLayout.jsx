d2087da05a067988be1e52819d207b9260b12eb8
PrinceRohithA <princerohith.a.2024.cse@rajalakshmi.edu.in>
2026-05-13 10:50:03 +0530

    v1

--- frontend/src/pages/GameDetailsSteamStyleLayout.jsx
+++ frontend/src/pages/GameDetailsSteamStyleLayout.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Link, useSearchParams } from 'react-router-dom';
 import { games as gamesApi, payments } from '../api';
 import RazorpayCheckout from '../components/RazorpayCheckout';
